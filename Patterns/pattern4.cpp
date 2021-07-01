8f18319b1ee2eb3ef6c9332f529248a3d67f187e
Gautam <gomzekh@gmail.com>
2021-07-01 05:44:36 +0530

    Pattern 4

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,40 @@
+/*
+•••••••••••••••••••••••••••••••
+Author: Gautam Khatter
+Date:   30 June 2021
+Title:  Pattern 4
+
+Pattern: 
+
+          * * * * *
+          * * * *
+          * * *
+          * *
+          *
+
+•••••••••••••••••••••••••••••••
+*/
+
+#include <iostream>
+
+int main()
+{
+  int i, j;
+  for ( i = 1; i <=5 ; i++)
+  {
+    for ( j = 1; j <=5 ; j++)
+    {
+      if(j <= 6-i)
+      {
+        std::cout << "*";
+      }
+      else
+      {
+        std::cout << " ";
+      }
+    }
+    std::cout << "\n";
+  }
+  
+  return 0;
+}
